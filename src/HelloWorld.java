70010abbe451a8255d96fd042c1f42e6a4d6b692
Lee1904 <77270969+Lee1904@users.noreply.github.com>
2021-01-12 13:05:04 +0000

    Update TIAGO1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world again");
-        System.out.println("Example TIAGO");
+        System.out.println("Ricardo");
         System.out.println("Tiago");
         System.out.println("Carla");
 
