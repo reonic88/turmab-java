ae41ba7def93187583f6b4ba37a8525b76754e20
Lee1904 <77270969+Lee1904@users.noreply.github.com>
2021-01-12 13:13:17 +0000

    Update TIAGO2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Ricardo");
-        System.out.println("Tiago");
-        System.out.println("Carla");
+        System.out.println("A");
+        System.out.println("B");
+        System.out.println("C");
+        System.out.println("D");
 
     }
 }
